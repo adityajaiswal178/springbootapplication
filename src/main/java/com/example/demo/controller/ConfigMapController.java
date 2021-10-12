b9cc5656eed1edc7464228eb11aaafc011d147e1
Aditya Jaiswal <51494265+adityajaiswal178@users.noreply.github.com>
2021-10-12 19:05:09 +0530

    Update ConfigMapController.java

--- src/main/java/com/example/demo/controller/ConfigMapController.java
+++ src/main/java/com/example/demo/controller/ConfigMapController.java
@@ -1,14 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class ConfigMapController {
+    @Value("${username}")
+    private String username;
+
 
     @GetMapping(value="/config")
     public String getConfig(){
-        return "My name= WIG";
+        return "Username is: "+username;
     }
 }
